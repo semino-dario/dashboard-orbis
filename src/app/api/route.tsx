beda37c49e6a2adcf08017648891a34e1984fa3d
Dario Semino <dariomartinsemino@gmail.com>
2024-07-25 16:06:41 -0300

    put corrected

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -32,61 +32,6 @@ export async function GET() {
     return ObjectId.isValid(id) && String(new ObjectId(id)) === id;
 }
 
-// export async function GET_BY_ID( id:string ) {
-//     try {
-
-//         if (!id) {
-//             return NextResponse.json({ error: 'the ID is required' }, { status: 400 });
-//         }
-   
-//         id = id.replace(/^"|"$/g, '');
-
-//         if (!isValidObjectId(id)) {
-//             return NextResponse.json({ error: 'ID must be a valid ObjectId' }, { status: 400 });
-//         }
-
-//         const { collection } = await connectToDatabase();
-//         const article = await collection.findOne({ _id: new ObjectId(id) });
-
-//         if (!article) {
-//             return NextResponse.json({ error: 'Article not found' }, { status: 404 });
-//         }
-
-//         return NextResponse.json(article, { status: 200 });
-//     } catch (error) {
-//         console.error('Error fetching article:', error);
-//         return NextResponse.json({ error: 'Failed to fetch article' }, { status: 500 });
-//     }
-// }
-
-// PUT method to update an article by ID
-export async function PUT(id:string, req: NextRequest) {
-    try {
-        if (!id) {
-            return NextResponse.json({ error: 'ID is required' }, { status: 400 });
-        }
-   
-        id = id.replace(/^"|"$/g, '');
-
-        if (!isValidObjectId(id)) {
-            return NextResponse.json({ error: 'ID must be a valid ObjectId' }, { status: 400 });
-        }
-
-        const data = await req.json();
-        const { collection } = await connectToDatabase();
-        const result = await collection.updateOne({ _id: new ObjectId(id) }, { $set: data });
-
-        if (result.matchedCount === 0) {
-            return NextResponse.json({ error: 'Article not found' }, { status: 404 });
-        }
-
-        return NextResponse.json(result, { status: 200 });
-    } catch (error) {
-        console.error('Error updating article:', error);
-        return NextResponse.json({ error: 'Failed to update article' }, { status: 500 });
-    }
-}
-
 // DELETE method to delete an article by ID
 export async function DELETE(req:NextRequest) {
     try {
