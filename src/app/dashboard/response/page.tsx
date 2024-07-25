22bda8663590505cb89586c7c9beaca9349370d9
Dario Semino <dariomartinsemino@gmail.com>
2024-07-25 16:10:38 -0300

    response corrected

--- src/app/dashboard/response/page.tsx
+++ src/app/dashboard/response/page.tsx
@@ -1,31 +1,11 @@
-import Link from "next/link";
 import styles from '../../page.module.css'
-// import { FetchLastArticle } from "../api/FetchData";
 
-interface ResponseProps{
-  id?: string;
-  title?:string
-}
-
-const Response:React.FC<ResponseProps> = async ({id, title})=> {
-
-    // const data:any = await FetchLastArticle()
-    // if (data) {
-    //   id = data.id
-    //   title = data.article.article[0].mainTitle
-    // }
+export default function Response (){
 
     return(
       <main className={styles.main}>
         <h2>lo hiciste Bo!</h2>
-        {/* <div className={styles.homeLinkContainer}>
-        <h1>Your article was created!</h1>
-      <Link href={`article/${id}/${title}`} className={styles.homeLink}>  <h3>Read article</h3> </Link>
-      <Link href="/all-articles" className={styles.homeLink}>  <h3>See all articles</h3></Link>
-        </div> */}
-
         </main>
     )
 }
 
-export default Response
