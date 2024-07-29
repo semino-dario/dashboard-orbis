4338e9cc805c8ecc8e64f67f8437cca109141abd
Dario Semino <dariomartinsemino@gmail.com>
2024-07-29 13:16:20 -0300

    fetch with use client in all-arts

--- src/app/dashboard/all-articles/page.tsx
+++ src/app/dashboard/all-articles/page.tsx
@@ -1,30 +1,62 @@
+"use client"
 import styles from "../../page.module.css";
 import AllArticles from "../../components/AllArticles";
-import { GET } from "@/app/api/route";
+import { useEffect, useState } from "react";
 
+export const fetchCache = 'force-no-store';
 
-export default  async function Articles (){
+export default function Articles (){
 
-let data
+  const  [data, setData] = useState([])
+  
+  useEffect( () => { async function getData () {
+    try{
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}`, {cache: 'no-store'});
+      const data =  await response.json()
+      setData(data)
+      return(data)
+    }
+     
+      catch (error){
+       console.log("Error IN Get all articles: ", error)
+     }} getData()}, []
+    )
+     
+    return (
+      <main className={styles.main}>
+          <div className="">
+            { data.length > 0 &&
+                 <AllArticles
+                 data={data}
+                 /> }
+          </div>
+   </main>
+    );
+  }
+  
 
-  try{
-    const response = await GET();
-    data = await response.json()}
+// export default  async function Articles (){
+
+// let data
+
+//   try{
+//     const response = await GET();
+//     data = await response.json()}
    
-    catch (error){
-     console.log("Error IN Get all articles: ", error)
-   }
+//     catch (error){
+//      console.log("Error IN Get all articles: ", error)
+//    }
    
    
-  return (
-    <main className={styles.main}>
-        <div className="">
-               <AllArticles
-               data={data}
-               /> 
-        </div>
- </main>
-  );
-}
+//   return (
+//     <main className={styles.main}>
+//         <div className="">
+//                <AllArticles
+//                data={data}
+//                /> 
+//         </div>
+//  </main>
+//   );
+// }
 
 
